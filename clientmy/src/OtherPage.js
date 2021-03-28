eae3a1caee020d4d1264cb12116970cb4dca9f2c
danilSpotad <dan861234567>
2021-03-29 00:06:35 +0300

    ddd

--- clientmy/src/OtherPage.js
+++ clientmy/src/OtherPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function default_own() {
-    return (
-        <div>
-            Im some other page!
-            <Link to="/">Go back home</Link>
-        </div>
-    );
-}
-
-export default default_own;
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
